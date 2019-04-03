3969096867ad842a88c25a067385ac59175cbb81
Howard Lander <howard@renci.org>
2019-04-03 18:37:09 +0000

    Added back the turnoff of power and screen saving

--- src/common/xfce/wm_startup.sh
+++ src/common/xfce/wm_startup.sh
@@ -5,9 +5,9 @@
 echo -e "\n------------------ startup of Xfce4 window manager ------------------"
 
 ### disable screensaver and power management
-#xset -dpms &
-#xset s noblank &
-#xset s off &
+xset -dpms &
+xset s noblank &
+xset s off &
 export $(dbus-launch)
 xfconf-query -c xfce4-session -p /startup/ssh-agent/enabled -n -t bool -s false
 xfconf-query -c xfce4-session -p /startup/gpg-agent/enabled -n -t bool -s false
