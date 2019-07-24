3e66c814be0bf6902d1077e64673ab196a6c39fd
Howard <howard@renci.org>
2019-07-24 12:34:43 -0400

    Fixed the inadvertant omission of these files

---
+++ public/config/docker_nginx-orthanc.js
@@ -0,0 +1,20 @@
+window.config = {
+  routerBasename: '/',
+  relativeWebWorkerScriptsPath: '',
+  servers: {
+    dicomWeb: [
+      {
+        name: 'Orthanc',
+        wadoUriRoot: '/wado',
+        qidoRoot: '/dicom-web',
+        wadoRoot: '/dicom-web',
+        qidoSupportsIncludeField: false,
+        imageRendering: 'wadors',
+        thumbnailRendering: 'wadors',
+        requestOptions: {
+          requestFromBrowser: true,
+        },
+      },
+    ],
+  },
+};
